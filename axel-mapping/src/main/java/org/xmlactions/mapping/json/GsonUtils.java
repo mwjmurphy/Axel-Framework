89f5fcc5ceeef3c5511c185d9afcea4d53b99727
mike <mwjmurphy@gmail.com>
2019-10-03 21:43:10 +0100

    fix json_get to return indexed lists

--- axel-mapping/src/main/java/org/xmlactions/mapping/json/GsonUtils.java
+++ axel-mapping/src/main/java/org/xmlactions/mapping/json/GsonUtils.java
@@ -11,9 +11,11 @@ import org.json.XML;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
 
 /**
  * @author mike
@@ -71,7 +73,7 @@ public class GsonUtils {
 		if (jsonElement.isJsonArray()) {
 			// loop until we reach index
 			JsonArray jsonArray = jsonElement.getAsJsonArray();
-			if (jsonArray.size() >= index) {
+			if (jsonArray.size() > index) {
 				jsonElement = jsonArray.get(index);
 				toMap(jsonElement, "", "", map);
 			} else {
@@ -189,59 +191,100 @@ public class GsonUtils {
 			}
 		}		
 	}
-	private static Object getObject(JSONObject jsonObject, String name) {
-		Object obj = jsonObject.get(name);
-		return obj;
+	private static Object getObject(JsonElement je, String name) {
+		if (je.isJsonArray()) {
+			JsonArray jsonArray = je.getAsJsonArray();
+			for (JsonElement jsonElement : jsonArray) {
+				if (jsonElement.isJsonObject()) {
+					JsonObject jsonObject = jsonElement.getAsJsonObject();
+					if (jsonObject.has(name)) {
+						return jsonObject.get(name);
+					}
+				}
+			}
+		}
+		if (je.isJsonObject()) {
+			JsonObject jsonObject = je.getAsJsonObject();
+			if (jsonObject.has(name)) {
+				return jsonObject.get(name);
+			}
+		}
+		if (je.isJsonPrimitive()) {
+			return null;
+			// JsonPrimitive jsonPrimitive = je.getAsJsonPrimitive();
+			// return jsonPrimitive;
+			
+		}
+		return null;
 	}
 
-	public static Object get(JSONObject jsonObject, String path) {
-		return (get(jsonObject, path, 0));
+	public static Object get(JsonElement jsonElement, String path) {
+		return (get(jsonElement, path, 0));
 	}
 
-	public static Object get(Object jObject, String path, int index) {
+	public static Object get(JsonElement jsonElement, String path, int index) {
 
 		if (path.length() == 0) {
-			return jObject;
+			return jsonElement;
 		}
 		String paths[] = path.split("/");
 		String workingPath = null;
+		boolean processedArray = false;
 		try {
 			for (int i = 0 ; i < paths.length; i++){
 				String name = paths[i];
 				workingPath = name;
-				if (jObject instanceof JSONArray) {
-					JSONArray jsonArray = (JSONArray) jObject;
+				if (jsonElement.isJsonArray()) {
+					JsonArray jsonArray = jsonElement.getAsJsonArray();
 					int counter = 0;
-					for (int iLoop = 0 ; iLoop < jsonArray.length(); iLoop++) {
-						jObject = jsonArray.get(iLoop);
-						if (jObject instanceof JSONObject) {
-							JSONObject jsonObject = (JSONObject) jObject;
-							if (jsonObject.has(workingPath)) {
+					for (int iLoop = 0 ; iLoop < jsonArray.size(); iLoop++) {
+						jsonElement = jsonArray.get(iLoop);
+						if (jsonElement.isJsonObject()) {
+							JsonObject jsonObject = jsonElement.getAsJsonObject();
+							if (jsonObject.has(name)) {
+								jsonElement = jsonObject.get(name);
 								counter++;
 								if (counter > index) {
+									processedArray = true;
 									break;
-									//Object o = jsonObject.get(workingPath);
-									//return o;
 								}
 							}
 						}
 					}
-				}
-				if (jObject instanceof JSONObject) {
-					JSONObject jsonObject = (JSONObject) jObject;
-					jObject = jsonObject.get(workingPath);
+				} else if (jsonElement.isJsonObject()) {
+					JsonObject jsonObject = jsonElement.getAsJsonObject();
+					if (jsonObject.has(name)) {
+						jsonElement = jsonObject.get(name);
+					} else {
+						return null;
+					}
+				} else if (jsonElement.isJsonPrimitive()) {
+					// what to do here
 				}
 			}
-			return jObject;
+			if (processedArray == false && jsonElement.isJsonArray()) {
+				JsonArray ja = jsonElement.getAsJsonArray();
+				int size = ja.size();
+				if (size > index) {
+					jsonElement = ja.get(index);
+				} else {
+					return null;	// outside index
+				}
+			}
+			if (jsonElement.isJsonPrimitive()) {
+				return jsonElement.getAsString();
+			}
+			return jsonElement;
 		} catch (Exception ex) {
-			throw new IllegalArgumentException("jObject[" + jObject + "] path[" + path + "] index[" + index + "] workingPath[" + workingPath + "]", ex);
+			throw new IllegalArgumentException("jObject[" + jsonElement + "] path[" + path + "] index[" + index + "] workingPath[" + workingPath + "]", ex);
 		}
 	}
 
 	public static Object get(String jsonString, String path, int index) {
 		try {
-			JSONObject jsonObject = new JSONObject(jsonString);
-			return get(jsonObject, path, index);
+			Gson gson = new Gson();
+			JsonElement jsonElement = gson.fromJson(jsonString, JsonElement.class);
+			return get(jsonElement, path, index);
 		} catch (Exception ex) {
 			logger.info("get error: jsonString:[{}] path:[{}] index[{}]", jsonString, path, index);
 			return null;
---
+++ axel-mapping/src/test/java/org/xmlactions/mapping/json/TestGsonUtils.java
@@ -0,0 +1,153 @@
+package org.xmlactions.mapping.json;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.XML;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.xmlactions.common.collections.PropertyKeyValue;
+import org.xmlactions.common.io.ResourceUtils;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
+public class TestGsonUtils {
+
+	private static final Logger logger = LoggerFactory.getLogger(TestGsonUtils.class);
+
+	@Test
+	public void testJsonObject() throws JSONException {
+		JsonObject obj = new JsonObject();
+		obj.addProperty("name", "Mike");
+		JsonObject child = new JsonObject();
+		child.addProperty("name", "Fred");
+		obj.add("child", child);
+		logger.debug(obj.toString());
+
+		JSONObject jsonObject = new JSONObject(obj.toString());
+		
+		String xml = XML.toString(jsonObject);
+		logger.debug(xml);
+		assertTrue(xml.contains("<child>"));
+		assertTrue(xml.contains("<name>Fred</name></child>"));
+
+	}
+
+	@Test
+	public void testJsonToMap() throws JSONException, IOException {
+		String jsonString = ResourceUtils.loadFile("/json/sample/data.json");
+		Gson gson = new Gson();
+		JsonElement jsonElement = gson.fromJson(jsonString, JsonElement.class);
+		for (int i = 0 ; ; i++) {
+			Map<String, Object> map = GsonUtils.toMap(jsonElement, "vessel_gear_types_category", i);
+			if (map != null) {
+				logger.debug("index:" + i);
+				dumpMap(map);
+			} else {
+				break;
+			}
+		}
+		logger.debug(jsonElement.toString());
+	}
+
+	@Test
+	public void testJsonToMap1() throws JSONException, IOException {
+		String jsonString = ResourceUtils.loadFile("/json/sample/json_with_array.json");
+		Gson gson = new Gson();
+		JsonElement jsonElement = gson.fromJson(jsonString, JsonElement.class);
+		for (int i = 0 ; ; i++) {
+			Map<String, Object> map = GsonUtils.toMap(jsonElement, "list", i);
+			if (map != null) {
+				logger.debug("index:" + i);
+				dumpMap(map);
+				logger.debug("row_index:" + map.get("row_index"));
+				logger.debug("list/PropertySearchResponse:" + map.get("list/PropertySearchResponse"));
+				logger.debug("PropertySearchResponse:" + map.get("PropertySearchResponse"));
+				
+			} else {
+				break;
+			}
+		}
+		logger.debug(jsonElement.toString());
+	}
+
+	@Test
+	public void testJsonToMapNested() throws JSONException, IOException {
+		Map<String, Object> map = new HashMap<String, Object>();
+		String jsonString = ResourceUtils.loadFile("/json/sample/data.json");
+		Gson gson = new Gson();
+		JsonElement jsonElement = gson.fromJson(jsonString, JsonElement.class);
+		Object obj = GsonUtils.get(jsonElement,"vessel_gear_types_category/title_text", 0);
+		assertEquals("Surrounding ", obj.toString());
+		obj = GsonUtils.get(jsonElement,"vessel_gear_types_category/title_text", 1);
+		assertEquals("Seine", obj.toString());
+	}
+
+	@Test
+	public void testJsonToMapNested2() throws JSONException, IOException {
+		Map<String, Object> map = new HashMap<String, Object>();
+		String jsonString = ResourceUtils.loadFile("/json/sample/json_with_array.json");
+		JSONObject jsonObject = new JSONObject(jsonString);
+		Object obj = JSONUtils.get(jsonObject,"list/PropertySearchResponse", 0);
+		// assertEquals("Surrounding ", obj.toString());
+		obj = JSONUtils.get(jsonObject,"list/PropertySearchResponse/PropertyResults/PropertyResult/Resort", 1);
+		assertEquals("Vilamoura", obj.toString());
+	}
+
+	@Test
+	public void testJsonToMap2() throws JSONException, IOException {
+		String jsonString = ResourceUtils.loadFile("/json/sample/root.json");
+		JSONObject jsonObject = new JSONObject(jsonString);
+		for (int i = 0 ; ; i++) {
+			Map<String, Object> map = JSONUtils.toMap(jsonObject, "root/vessel_gear_types_category", i);
+			if (map != null) {
+				logger.debug("index:" + i);
+				dumpMap(map);
+			} else {
+				// completed iteration through all path elements
+				break;
+			}
+		}
+		logger.debug(jsonObject.toString());
+	}
+	
+	@Test
+	public void testJsonValue() throws JSONException, IOException {
+		String jsonString = ResourceUtils.loadFile("/json/sample/hotel.json");
+		Gson gson = new Gson();
+		JsonElement jsonElement = gson.fromJson(jsonString, JsonElement.class);
+		Object obj = GsonUtils.get(jsonElement, "location/type");
+		assertEquals("Point", obj.toString());
+		obj = GsonUtils.get(jsonElement, "location/coordinates");
+		assertEquals("13.894", obj.toString());
+		obj = GsonUtils.get(jsonElement, "location/coordinates", 0);
+		assertEquals("13.894", obj.toString());
+		obj = GsonUtils.get(jsonElement, "location/coordinates", 1);
+		assertEquals("40.6972", obj.toString());
+		obj = GsonUtils.get(jsonElement, "location/coordinates", 2);
+		assertEquals(null, obj);
+	}
+	
+	private void dumpMap(Map<String, Object> map) {
+		Set set = map.entrySet();
+		Iterator iterator = set.iterator();
+		while(iterator.hasNext()) {
+			Object obj = iterator.next();
+			logger.debug("obj:" + obj);
+		}
+	}
+
+}
