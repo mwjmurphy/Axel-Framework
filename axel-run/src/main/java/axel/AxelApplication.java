59291aa4569ab40daee243def260991407f8aece
mike <mwjmurphy@gmail.com>
2020-04-19 12:06:59 +0100

    Remove sourcsets from axel-jar

--- axel-run/src/main/java/axel/AxelApplication.java
+++ axel-run/src/main/java/axel/AxelApplication.java
@@ -1,31 +1,11 @@
 package axel;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.Arrays;
 
-import javax.servlet.Servlet;
-import javax.servlet.ServletConfig;
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.apache.commons.fileupload.FileUploadException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpRequest;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.ServletConfigAware;
-import org.springframework.web.context.ServletContextAware;
-import org.xmlactions.action.config.IExecContext;
-import org.xmlactions.web.PagerServlet;
-import org.xmlactions.web.RequestExecContext;
-import org.xmlactions.web.conceal.HttpPager;
 
 @SpringBootApplication
 @RestController
--- axel-run/src/main/java/axel/AxelController.java
+++ axel-run/src/main/java/axel/AxelController.java
@@ -1,29 +1,22 @@
 package axel;
 
-import java.io.File;
-import java.io.IOException;
-
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.fileupload.FileUploadException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.context.ServletConfigAware;
 import org.springframework.web.context.ServletContextAware;
-import org.xmlactions.action.config.IExecContext;
-import org.xmlactions.web.PagerFilter;
+import org.xmlactions.mapping.json.GsonUtils;
 import org.xmlactions.web.PagerServlet;
-import org.xmlactions.web.RequestExecContext;
 import org.xmlactions.web.conceal.HttpPager;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
 
 @Controller
 @RequestMapping
@@ -76,5 +69,11 @@ public class AxelController implements ServletContextAware, ServletConfigAware {
 	    return new ServletRegistrationBean(pagerServlet, "/axel/*");
 	}
 
+	private void test() {
+		GsonUtils gu;
+		String jsonString = "{\"name\":\"Fred\"}";
+		Gson gson = new Gson();
+		JsonElement jsonElement = gson.fromJson(jsonString, JsonElement.class);
+	}
 
 }
